4a2c4b89b205b6c6ef81bafea0b3979c28bacf7f
Rene Spies <rene.j.spies@gmail.com>
2020-08-14 21:13:28 +0200

    Added a speed dial menu to the FloatingActionButton using a third party library.

--- app/src/main/java/co/aresid/book13/activity/MainActivity.kt
+++ app/src/main/java/co/aresid/book13/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package co.aresid.book13.activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import co.aresid.book13.R
 import co.aresid.book13.databinding.ActivityMainBinding
 
 class MainActivity: AppCompatActivity() {
@@ -16,6 +17,9 @@ class MainActivity: AppCompatActivity() {
 
 		// Define the binding and inflate the layout
 		binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+
+		// TODO: 8/14/20 Change speed dial's items backgroundColor or iconTint
+		binding.speedDialView.inflate(R.menu.speed_dial)
 
 		// Set the inflated layout
 		setContentView(binding.root)
