2039c299a60ec0279dfb42217a36c60cbec382de
Rene Spies <rene.j.spies@gmail.com>
2020-08-14 20:21:08 +0200

    Removed AppBarLayout's elevation and added the DataBinding to the MainActivity.kt file.

--- app/src/main/java/co/aresid/book13/activity/MainActivity.kt
+++ app/src/main/java/co/aresid/book13/activity/MainActivity.kt
@@ -1,15 +1,24 @@
 package co.aresid.book13.activity
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
-import co.aresid.book13.R
+import co.aresid.book13.databinding.ActivityMainBinding
 
 class MainActivity: AppCompatActivity() {
+
+	// Binding for the layout
+	private lateinit var binding: ActivityMainBinding
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_main)
+
+		// Define the binding and inflate the layout
+		binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+
+		// Set the inflated layout
+		setContentView(binding.root)
 
 	}
 
