7bbbbd2f1cd05479955f3298a381f76172b201bd
Rene Spies <rene.j.spies@gmail.com>
2020-08-16 16:02:16 +0200

    Added SpeedDial's onClickListener and logic.

--- app/src/main/java/co/aresid/book13/activity/MainActivity.kt
+++ app/src/main/java/co/aresid/book13/activity/MainActivity.kt
@@ -3,6 +3,7 @@ package co.aresid.book13.activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import co.aresid.book13.R
 import co.aresid.book13.databinding.ActivityMainBinding
 
@@ -18,8 +19,34 @@ class MainActivity: AppCompatActivity() {
 		// Define the binding and inflate the layout
 		binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
 
-		// TODO: 8/14/20 Change speed dial's items backgroundColor or iconTint
+		// Inflate the menu for the SpeedDial
 		binding.speedDialView.inflate(R.menu.speed_dial)
+
+		val navigationHostFragment =
+			supportFragmentManager.findFragmentById(R.id.navigation_host) as NavHostFragment
+		val navigationController = navigationHostFragment.navController
+
+		binding.speedDialView.setOnActionSelectedListener { actionItem ->
+
+			when (actionItem.id) {
+
+				R.id.add_book -> {
+
+					navigationController.navigate(R.id.addBookFragment)
+
+				}
+
+				R.id.start_tracking -> {
+
+					navigationController.navigate(R.id.trackingDetailsFragment)
+
+				}
+
+			}
+
+			false
+
+		}
 
 		// Set the inflated layout
 		setContentView(binding.root)
