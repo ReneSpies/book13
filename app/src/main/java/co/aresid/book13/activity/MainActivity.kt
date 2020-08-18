c49f37404929e0e1eb083aeae13b6eadcc8359e8
Rene Spies <rene.j.spies@gmail.com>
2020-08-18 15:25:33 +0200

    Removed the disappear behaviour of the FAB and added pop-up-to and animations to the navigation using the FAB's speed dial.

--- app/src/main/java/co/aresid/book13/activity/MainActivity.kt
+++ app/src/main/java/co/aresid/book13/activity/MainActivity.kt
@@ -3,54 +3,64 @@ package co.aresid.book13.activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.NavHostFragment
 import co.aresid.book13.R
 import co.aresid.book13.databinding.ActivityMainBinding
 
-class MainActivity: AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
-	// Binding for the layout
-	private lateinit var binding: ActivityMainBinding
+    // Binding for the layout
+    private lateinit var binding: ActivityMainBinding
 
-	override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
 
-		super.onCreate(savedInstanceState)
+        super.onCreate(savedInstanceState)
 
-		// Define the binding and inflate the layout
-		binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
+        // Define the binding and inflate the layout
+        binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
 
-		// Inflate the menu for the SpeedDial
-		binding.speedDialView.inflate(R.menu.speed_dial)
+        // Inflate the menu for the SpeedDial
+        binding.speedDialView.inflate(R.menu.speed_dial)
 
-		val navigationHostFragment =
-			supportFragmentManager.findFragmentById(R.id.navigation_host) as NavHostFragment
-		val navigationController = navigationHostFragment.navController
+        val navigationHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navigation_host) as NavHostFragment
+        val navigationController = navigationHostFragment.navController
 
-		binding.speedDialView.setOnActionSelectedListener { actionItem ->
+        binding.speedDialView.setOnActionSelectedListener { actionItem ->
 
-			when (actionItem.id) {
+            val navigationOptions =
+                NavOptions.Builder().setPopUpTo(R.id.trackingListFragment, false)
+                    .setEnterAnim(R.anim.fade_in).setPopEnterAnim(R.anim.fade_in)
+                    .setExitAnim(R.anim.fade_out).setPopExitAnim(R.anim.fade_out).build()
 
-				R.id.add_book -> {
+            when (actionItem.id) {
 
-					navigationController.navigate(R.id.addBookFragment)
+                R.id.add_book -> {
 
-				}
+                    navigationController.navigate(R.id.addBookFragment, null, navigationOptions)
 
-				R.id.start_tracking -> {
+                }
 
-					navigationController.navigate(R.id.trackingDetailsFragment)
+                R.id.start_tracking -> {
 
-				}
+                    navigationController.navigate(
+                        R.id.trackingDetailsFragment,
+                        null,
+                        navigationOptions
+                    )
 
-			}
+                }
 
-			false
+            }
 
-		}
+            false
 
-		// Set the inflated layout
-		setContentView(binding.root)
+        }
 
-	}
+        // Set the inflated layout
+        setContentView(binding.root)
+
+    }
 
 }
