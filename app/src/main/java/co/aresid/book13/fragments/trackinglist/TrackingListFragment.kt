bb8c160f3c480e2590c8d91eb8048893e0c17d52
René Jörg Spies <rene.j.spies@gmail.com>
2021-04-03 15:34:39 +0200

    Revert "Feature: Clicking a RecyclerView item now navigates to a fragment showing its title (for now)"
    This reverts commit f293d9d7bab7fe8b149fa9a4f06c9703ae22168e.
    
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/java/co/aresid/book13/fragments/trackinglist/TrackingListFragment.kt
    #	app/src/main/java/co/aresid/book13/fragments/trackinglist/TrackingListViewModel.kt
    #	app/src/main/java/co/aresid/book13/recyclerview/TrackingListAdapter.kt
    #	app/src/main/java/co/aresid/book13/recyclerview/TrackingListViewHolder.kt
    #	app/src/main/res/layout/item_tracking_list.xml
    #	app/src/main/res/navigation/navigation_graph.xml
    #	build.gradle

--- app/src/main/java/co/aresid/book13/fragments/trackinglist/TrackingListFragment.kt
+++ app/src/main/java/co/aresid/book13/fragments/trackinglist/TrackingListFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.ItemTouchHelper
 import co.aresid.book13.databinding.FragmentTrackingListBinding
 import co.aresid.book13.recyclerview.TrackingListAdapter
 import timber.log.Timber
@@ -18,51 +19,60 @@ import timber.log.Timber
  */
 
 class TrackingListFragment: Fragment() {
-	
-	// Binding for the layout
-	private lateinit var binding: FragmentTrackingListBinding
-	
-	// Corresponding ViewModel
-	private lateinit var trackingListViewModel: TrackingListViewModel
-	
-	override fun onCreateView(
-		inflater: LayoutInflater,
-		container: ViewGroup?,
-		savedInstanceState: Bundle?
-	): View {
-		
-		Timber.d("onCreateView: called")
-		
-		super.onCreateView(
-			inflater,
-			container,
-			savedInstanceState
-		)
-		
-		// Define the binding and inflate the layout
-		binding = FragmentTrackingListBinding.inflate(
-			inflater,
-			container,
-			false
-		)
-		
-		// Define the ViewModel
-		trackingListViewModel = ViewModelProvider(this).get(TrackingListViewModel::class.java)
-		
-		binding.lifecycleOwner = viewLifecycleOwner
-		binding.viewModel = trackingListViewModel
-		binding.allTrackingData = trackingListViewModel.allTrackingData
-		binding.trackingListRecyclerView.adapter = TrackingListAdapter(listOf())
-		
-		trackingListViewModel.allTrackingData.observe(viewLifecycleOwner) {
-			
-			(binding.trackingListRecyclerView.adapter as TrackingListAdapter).setTrackingList(it)
-			
-		}
-		
-		// Return the inflated layout
-		return binding.root
-		
-	}
-	
+
+    // Binding for the layout
+    private lateinit var binding: FragmentTrackingListBinding
+
+    // Corresponding ViewModel
+    private lateinit var trackingListViewModel: TrackingListViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        Timber.d("onCreateView: called")
+
+        super.onCreateView(
+            inflater,
+            container,
+            savedInstanceState
+        )
+
+        // Define the binding and inflate the layout
+        binding = FragmentTrackingListBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+
+        // Define the ViewModel
+        trackingListViewModel = ViewModelProvider(this).get(TrackingListViewModel::class.java)
+
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.viewModel = trackingListViewModel
+        binding.allTrackingData = trackingListViewModel.allTrackingData
+
+        trackingListViewModel.allTrackingData.observe(viewLifecycleOwner) {
+
+            binding.trackingListRecyclerView.adapter = TrackingListAdapter(it)
+
+        }
+
+        trackingListViewModel.itemTouchHelperCallback.observe(viewLifecycleOwner) {
+
+            it?.apply {
+
+                ItemTouchHelper(this).attachToRecyclerView(binding.trackingListRecyclerView)
+
+            }
+
+        }
+
+        // Return the inflated layout
+        return binding.root
+
+    }
+
 }
--- app/src/main/java/co/aresid/book13/recyclerview/TrackingListViewHolder.kt
+++ app/src/main/java/co/aresid/book13/recyclerview/TrackingListViewHolder.kt
@@ -1,12 +1,9 @@
 package co.aresid.book13.recyclerview
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import co.aresid.book13.databinding.ItemTrackingListBinding
-import co.aresid.book13.fragments.trackinglist.TrackingListFragmentDirections
 import timber.log.Timber
 
 /**
@@ -16,35 +13,26 @@ import timber.log.Timber
  *    Copyright: © 2020 ARES ID
  */
 
-class TrackingListViewHolder private constructor(val binding: ItemTrackingListBinding): RecyclerView.ViewHolder(binding.root) {
-	
-	companion object {
-		
-		fun from(parent: ViewGroup): TrackingListViewHolder {
-			
-			Timber.d("from: called")
-			
-			val layoutInflater = LayoutInflater.from(parent.context)
-			val binding = ItemTrackingListBinding.inflate(
-				layoutInflater,
-				parent,
-				false
-			)
-			
-			return TrackingListViewHolder(binding)
-			
-		}
-		
-	}
-	
-	fun itemClicked(view: View) {
-		
-		Timber.d("itemClicked: called")
-		
-		val action = TrackingListFragmentDirections.toStartTrackingFragment(binding.trackingData ?: return)
-		
-		view.findNavController().navigate(action)
-		
-	}
-	
+class TrackingListViewHolder private constructor(val binding: ItemTrackingListBinding):
+    RecyclerView.ViewHolder(binding.root) {
+
+    companion object {
+
+        fun from(parent: ViewGroup): TrackingListViewHolder {
+
+            Timber.d("from: called")
+
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val binding = ItemTrackingListBinding.inflate(
+                layoutInflater,
+                parent,
+                false
+            )
+
+            return TrackingListViewHolder(binding)
+
+        }
+
+    }
+
 }
